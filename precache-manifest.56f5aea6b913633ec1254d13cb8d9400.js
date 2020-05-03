1f849a7193c1a4f7d45852c03082feebca10d351
Qhristen <odeyxtian@gmail.com>
2020-05-03 15:47:30 +0100

    Updates

---
+++ precache-manifest.56f5aea6b913633ec1254d13cb8d9400.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b83f77d7c577ed462ee08e027fcf8aa7",
+    "url": "/TextWrite/index.html"
+  },
+  {
+    "revision": "335cda97e2b741dda7d1",
+    "url": "/TextWrite/static/css/main.c71c138f.chunk.css"
+  },
+  {
+    "revision": "b1beb972ca980335c5a2",
+    "url": "/TextWrite/static/js/2.272d7c7b.chunk.js"
+  },
+  {
+    "revision": "f92b1fe87bc73201e8e452e0fe40dfef",
+    "url": "/TextWrite/static/js/2.272d7c7b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "335cda97e2b741dda7d1",
+    "url": "/TextWrite/static/js/main.cef76765.chunk.js"
+  },
+  {
+    "revision": "ec230f70fe848512a94d",
+    "url": "/TextWrite/static/js/runtime-main.ddfc43de.js"
+  },
+  {
+    "revision": "d768d5848a13df68dd39980a2e522250",
+    "url": "/TextWrite/static/media/Fattana-Regular.d768d584.otf"
+  },
+  {
+    "revision": "3e0d7f07951a63f4480da72e5f74a5db",
+    "url": "/TextWrite/static/media/bg.3e0d7f07.svg"
+  }
+]);
